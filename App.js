5ed18df5127b7e96ae4fdd2150849a071674bf00
heliannika <heliannikar@outlook.com>
2023-10-24 13:36:22 +0300

    setresultcolor added

--- App.js
+++ App.js
@@ -1,18 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, Switch, Text, TextInput, View } from 'react-native';
+import { Pressable, ScrollView, Switch, Text, TextInput, View, Alert } from 'react-native';
 import Styles, { lightTheme, darkTheme } from './styles/Style';
 import { useState } from 'react';
 import NumericInput from 'react-native-numeric-input';
+import { RadioButton } from 'react-native-paper';
 
 export default function App() {
 
   const [theme, setTheme] = useState(false);
   const [weight, setWeight] = useState(0);
   const [bottles, setBottles] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [BAClevel, setBAClevel] = useState(0);
+  // const [BAClevelMessage, setBAClevelMessage] = useState('');
+  const [message, setMessage] = useState('');
+  const [resultColor, setResultColor] = useState('');
+
+  const genders = ['female', 'male'];
+
+  const [gender, setGender] = useState(genders[0]);
 
   const currentTheme = theme ? lightTheme : darkTheme;
 
-  console.log(bottles);
+  // Function to calculate the alcohol level.
+
+  const calculate = () => {
+
+    /* Alerting if weight is not typed, it is not typed in numbers or it is empty. 
+    If the weight input is done correctly, then it will do the calculation. */
+
+    if (isNaN(weight) || weight == 0 || weight === '') {
+      alert('Weight cannot be empty, a zero or not a number.');
+    }
+    else {
+      let litres = bottles * 0.33;
+      let grams = litres * 8 * 4.5;
+      let burning = weight / 10;
+      let gramsleft = grams - burning * hours;
+      let result = 0;
+
+      if (gender === 'female') {
+        result = (gramsleft / (weight * 0.6));
+      } else if ( gender === 'male') {
+        result = (gramsleft / (weight * 0.7));
+      }
+
+      // Colors for the BAC level output.
+
+      if (result < 0.2) {
+        setResultColor('green');
+      }
+      else if (result < 0.5 && result > 0.2) {
+        setResultColor('yellow');
+      }
+      else {
+        setResultColor('red');
+      }
+
+      // Message given if the BAC level is below zero.
+
+      if (result < 0) {
+        setMessage('BAC level cannot be negative');
+      }
+
+      setBAClevel(result);
+    }
+  }
 
   return (
     <ScrollView style={currentTheme.container}>
@@ -30,21 +83,54 @@ export default function App() {
         <Text style={currentTheme.subheader}>Weight</Text>
         <TextInput
           style={currentTheme.textInput} 
-         /*
           keyboardType='number-pad'
-          value={weight}
-          onChangeText={setWeight} */
+          onChangeText={setWeight}
         />
+      </View>
+      <View style={currentTheme.numInputView}>
+        <View>
+          <Text style={currentTheme.subheader}>Bottles</Text>
+          <NumericInput
+            rightButtonBackgroundColor= '#df7272ff'
+            leftButtonBackgroundColor= '#df7272ff'
+            borderColor= '#df7272ff'
+            minValue={0}
+            value={bottles}
+            onChange={setBottles}
+          />
+        </View>
+        <View>
+          <Text style={currentTheme.subheader}>Hours</Text>
+          <NumericInput
+            rightButtonBackgroundColor= '#df7272ff'
+            leftButtonBackgroundColor= '#df7272ff'
+            borderColor= '#df7272ff'
+            minValue={0}
+            value={hours}
+            onChange={setHours}
+          />
+        </View>
       </View>
       <View>
-        <Text style={currentTheme.subheader}>Bottles</Text>
-        <NumericInput
-          style={currentTheme.bottleInput}
-          minValue={0}
-          value={bottles}
-          onChange={setBottles}
-        />
+        <RadioButton.Group value={gender} onValueChange={g => setGender(g)}>
+          {genders.map (g =>
+            <View key={g} 
+            style={currentTheme.radioButtons}>
+              <RadioButton value={g} />
+              <Text>{g}</Text>
+            </View>
+            )}
+        </RadioButton.Group>
       </View>
+      <Text>{message}</Text>
+      <View>
+        <Text>BAC level is: </Text>
+        <Text style={[{color: resultColor}]}>{BAClevel.toFixed(2)}</Text>
+      </View>
+      <Pressable
+        onPress={() => calculate()}>
+        <Text>Calculate</Text>
+      </Pressable>
     </ScrollView>
   );
 }
--- styles/Style.js
+++ styles/Style.js
@@ -1,6 +1,5 @@
 import { StyleSheet } from "react-native";
 import Constants from "expo-constants";
-import NumericInput from 'react-native-numeric-input';
 
 
 const lightTheme = StyleSheet.create({
@@ -8,22 +7,22 @@ const lightTheme = StyleSheet.create({
       flex: 1,
       backgroundColor: '#ed9d9de5',
       marginTop: Constants.statusBarHeight+10,
+      padding: 20
     },
     switchRow:{
       flexDirection: 'row',
       alignItems: 'center',
-      padding: 5,
-      alignSelf: "flex-end"
+      alignSelf: 'flex-end'
     },
     switch:{
-      alignSelf:"flex-start",
+      alignSelf: 'flex-start',
     },
     header:{
       fontSize: 50,
       fontFamily: 'sans-serif-thin',
       color: '#8b3636ed',
       textAlign: 'center',
-      padding: 10
+      padding: 5
     },
     text:{
       fontFamily: 'sans-serif-thin',
@@ -34,20 +33,24 @@ const lightTheme = StyleSheet.create({
       color: '#8b3636ed',
       fontSize: 25,
       textAlign: 'left',
-      padding: 10
+      marginTop: 10,
+      marginBottom: 10
     },
     textInput:{
       backgroundColor: '#8b3636ed',
-      margin: 12,
       borderRadius: 5,
       borderColor: '#853636c1',
       borderWidth: 2,
-      padding: 5,
       color: '#ed9d9de5',
-      fontFamily: 'sans-serif-thin'
+      fontFamily: 'sans-serif-thin',
+      marginBottom: 10,
+      paddingLeft: 5
     },
-    bottleInput:{
-      borderColor: '#8b3636ed'
+    numInputView:{
+      alignItems: 'center',
+    },
+    radioButtons:{
+      justifyContent: 'space-around',
     },
   });
 
@@ -56,11 +59,11 @@ const darkTheme = StyleSheet.create({
       flex: 1,
       backgroundColor: '#8b3636ed',
       marginTop: Constants.statusBarHeight+10,
+      padding: 20
     },
     switchRow:{
       flexDirection: 'row',
       alignItems: 'center',
-      padding: 5,
       alignSelf: "flex-end"
     },
     switch:{
@@ -71,7 +74,7 @@ const darkTheme = StyleSheet.create({
       fontFamily: 'sans-serif-thin',
       color: '#ed9d9de5',
       textAlign: 'center',
-      padding: 10
+      padding: 5
     },
     text:{
       fontFamily: 'sans-serif-thin',
@@ -82,17 +85,29 @@ const darkTheme = StyleSheet.create({
       color: '#ed9d9de5',
       fontSize: 25,
       textAlign: 'left',
-      padding: 10
+      marginTop: 10,
+      marginBottom: 10
     },
     textInput:{
       backgroundColor: '#ed9d9de5',
-      margin: 12,
       borderRadius: 5,
       borderColor: '#853636c1',
       borderWidth: 2,
-      padding: 5,
       color: '#8b3636ed',
-      fontFamily: 'sans-serif-thin'
+      fontFamily: 'sans-serif-thin',
+      marginBottom: 10,
+      paddingLeft: 5
+    },
+    numInputView:{
+      alignItems: 'center',
+    },
+    radioButtons:{
+      flexDirection: 'row',
+      alignItems: 'center',
+      color: '#ed9d9de5'
+    },
+    result:{
+      
     }
   });
 
